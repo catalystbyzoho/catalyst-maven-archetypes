cdbfee6d5b4d04a0ddc963acbff4c7dba2930498
thangaram.s <thangaram.s@zohocorp.com>
2022-07-05 18:16:54 +0530

    initial commit

---
+++ catalyst-archetype-advancedio/src/main/resources/archetype-resources/src/main/java/__className__.java
@@ -0,0 +1,33 @@
+package $package;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.catalyst.advanced.CatalystAdvancedIOHandler;
+
+import com.zc.common.ZCProject;
+import com.zc.component.cache.ZCCache;
+
+public class ${className} implements CatalystAdvancedIOHandler
+{
+    private static final Logger LOGGER = Logger.getLogger(${className}.class.getName());
+	
+	@Override
+    public void runner(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
+			ZCProject.initProject();
+			String name = (String) request.getParameter("name");
+			LOGGER.log(Level.INFO, "Hello " + name);
+			ZCCache.getInstance().putCacheValue("${className}", "Working", 1l);
+			response.setStatus(200);
+		}
+		catch(Exception e) {
+			LOGGER.log(Level.SEVERE,"Exception in ${className}",e);
+			response.setStatus(500);
+		}
+        response.getWriter().write("Hello From ${className}.java");
+	}
+}
---
+++ catalyst-archetype-advancedio/src/main/resources/archetype-resources/src/test/java/__className__Test.java
@@ -0,0 +1,20 @@
+package $package;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ${className}Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
